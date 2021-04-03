519edc324f5f97cfd1d69e0a9208f77dd9f73a08
Zach Huxford <zachbwh@gmail.com>
2021-04-03 18:26:33 +1300

    nicknames definition moved into the conversation interface file

--- src/typescript-types/Messages/IConversation.tsx
+++ src/typescript-types/Messages/IConversation.tsx
@@ -4,11 +4,16 @@ interface IConversation {
 	ConversationImage: string,
 	ConversationImageAltText?: string,
 	Participants: string[],
-	NickNames?: string[]
+	NickNames?: INickName[]
 	ConversationName?: string,
 	LastActiveDate: Date,
 	Messages: string[],
 	DraftMessage?: DraftMessage
+};
+
+export interface INickName {
+	UserId: string,
+	NickName: string
 };
 
 export default IConversation;
--- src/typescript-types/Messages/INickName.tsx
+++
@@ -1,6 +0,0 @@
-interface INickName {
-	UserId: string,
-	NickName: string
-};
-
-export default INickName;
